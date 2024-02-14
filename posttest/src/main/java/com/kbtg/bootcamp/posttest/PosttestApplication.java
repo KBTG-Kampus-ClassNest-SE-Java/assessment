6c7b686aedbc7ba91ddc58e88d63e50a12a6351e
bhuridech <aorjor.bootcamp@gmail.com>
2024-02-14 23:34:25 +0700

    add java initial project

---
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/PosttestApplication.java
@@ -0,0 +1,13 @@
+package com.kbtg.bootcamp.posttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosttestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosttestApplication.class, args);
+	}
+
+}
