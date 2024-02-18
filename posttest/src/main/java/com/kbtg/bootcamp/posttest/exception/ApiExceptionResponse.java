1e62720c2782467c8310250e92a4ad7020d0843b
Phongpanot N <phongpanot.ngo@ascendcorp.com>
2024-02-18 15:43:47 +0700

    Update ApiExceptionResponse.java

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/exception/ApiExceptionResponse.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/exception/ApiExceptionResponse.java
@@ -1,28 +1,8 @@
 package com.kbtg.bootcamp.posttest.exception;
 
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 import java.time.ZonedDateTime;
 
-public class ApiExceptionResponse {
-    @Getter
-    private final String message;
-
-    @Getter
-    private final HttpStatus httpStatus;
-
-    @Getter
-    private final ZonedDateTime dateTime;
-
-    public ApiExceptionResponse(
-            String message,
-            HttpStatus httpStatus,
-            ZonedDateTime dateTime
-    ) {
-        this.message = message;
-        this.httpStatus = httpStatus;
-        this.dateTime = dateTime;
-    }
-
+public record ApiExceptionResponse(String message, HttpStatus httpStatus, ZonedDateTime dateTime) {
 }
