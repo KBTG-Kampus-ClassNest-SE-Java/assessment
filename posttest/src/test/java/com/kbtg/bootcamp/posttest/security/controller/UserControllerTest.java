9937918340f71206217efa6b094f606b4119ca8e
global <global@mail.com>
2024-02-19 18:06:58 +0700

    failed test shouldNotReturnStatusOk in UserControllerTest

--- posttest/src/test/java/com/kbtg/bootcamp/posttest/security/controller/UserControllerTest.java
+++ posttest/src/test/java/com/kbtg/bootcamp/posttest/security/controller/UserControllerTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(
@@ -19,5 +21,6 @@ class UserControllerTest {
     void shouldNotReturnStatusOk() {
         ResponseEntity<String> response =
             restTemplate.getForEntity("/users", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     }
 }
