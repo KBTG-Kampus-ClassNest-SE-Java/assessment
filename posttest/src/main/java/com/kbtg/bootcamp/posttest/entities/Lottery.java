39c0c4db7f67676eb4aa2d1503c2c1d9d8a330cd
errortime <larb26656@gmail.com>
2024-02-19 22:04:24 +0700

    Feat (add database design and entities)

---
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/entities/Lottery.java
@@ -0,0 +1,21 @@
+package com.kbtg.bootcamp.posttest.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Data
+public class Lottery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 6)
+    private String ticketId;
+
+    private Integer qty;
+
+    private BigDecimal price;
+}
