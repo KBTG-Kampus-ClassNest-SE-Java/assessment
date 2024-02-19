b570a803d853cdc18b4e0a74e12086deb0616b34
global <global@mail.com>
2024-02-19 18:59:20 +0700

    failed shouldNotReturnEntity TestLotteryControllerTest.class

--- posttest/src/test/java/com/kbtg/bootcamp/posttest/security/controller/test/TestLotteryControllerTest.java
+++ posttest/src/test/java/com/kbtg/bootcamp/posttest/security/controller/test/TestLotteryControllerTest.java
@@ -1,6 +1,7 @@
 package com.kbtg.bootcamp.posttest.security.controller.test;
 
 import com.kbtg.bootcamp.posttest.lottery.Lottery;
+import com.kbtg.bootcamp.posttest.lottery.LotteryRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,11 +19,20 @@ class TestLotteryControllerTest {
     @Autowired
     TestRestTemplate restTemplate;
 
+    @Autowired
+    LotteryRepository lotteryRepository;
+
     @Test
     void shouldNotReturnHttpStatusOK() {
         ResponseEntity<String> response =
                 restTemplate.getForEntity("/test", String.class);
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
+    }
+
+    @Test
+    void shouldNotReturnEntity() {
+        Lottery lottery = lotteryRepository.findById(1L).orElseThrow(RuntimeException::new); // right id
+        assertThat(lottery).isNull();
     }
 }
