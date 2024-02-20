958767c7e38a3c8162a9eb685abe66dfa5e9e7b4
Pantakarn Kowdokrak <peachoenixz@gmail.com>
2024-02-20 15:13:22 +0700

    feat(Lottery) : add constant variable file

---
+++ posttest/src/main/java/com/kbtg/bootcamp/lottery/lib/Const.java
@@ -0,0 +1,7 @@
+package com.kbtg.bootcamp.lottery.lib;
+
+public final class Const {
+    public static final String LOTTERY_NOT_SOLD_BACK_FLAG = "N";
+    public static final String LOTTERY_SOLD_OUT_MESSAGE_FORMAT = "Lottery number: %s sold out";
+    public static final String LOTTERY_NOT_FOUND = "Lottery number: %s not found.";
+}
