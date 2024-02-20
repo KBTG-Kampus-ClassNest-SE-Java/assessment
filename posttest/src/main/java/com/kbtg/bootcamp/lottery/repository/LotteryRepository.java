a1ee76d4fafc175e70c18c268f4e34fc7e0e86cc
Pantakarn Kowdokrak <peachoenixz@gmail.com>
2024-02-20 15:13:51 +0700

    feat(Lottery) : change design DB

--- posttest/src/main/java/com/kbtg/bootcamp/lottery/repository/LotteryRepository.java
+++ posttest/src/main/java/com/kbtg/bootcamp/lottery/repository/LotteryRepository.java
@@ -3,6 +3,6 @@ package com.kbtg.bootcamp.lottery.repository;
 import com.kbtg.bootcamp.lottery.entity.Lottery;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LotteryRepository extends JpaRepository<Lottery, Integer> {
+public interface LotteryRepository extends JpaRepository<Lottery, String> {
 
 }
