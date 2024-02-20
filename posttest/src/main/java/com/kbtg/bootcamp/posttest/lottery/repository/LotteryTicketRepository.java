868c541e7f694a85ab23c69449d97754f95a6c21
Phongpanot N <phongpanot.ngo@ascendcorp.com>
2024-02-21 00:15:15 +0700

    Update LotteryTicketRepository.java

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/lottery/repository/LotteryTicketRepository.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/lottery/repository/LotteryTicketRepository.java
@@ -4,6 +4,9 @@ import com.kbtg.bootcamp.posttest.lottery.model.LotteryTicket;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LotteryTicketRepository extends JpaRepository<LotteryTicket, Long> {
+    Optional<LotteryTicket> findByTicket(String ticket);
 }
