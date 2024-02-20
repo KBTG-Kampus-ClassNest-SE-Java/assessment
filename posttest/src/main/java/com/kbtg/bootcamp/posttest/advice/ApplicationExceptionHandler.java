f7ca7f4c4c90950a5c4f4050cb6e3c2bf62ac6fc
zhunismp <kobkit.zhun@gmail.com>
2024-02-21 00:18:51 +0700

    adding handler for invalid path variable

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/advice/ApplicationExceptionHandler.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/advice/ApplicationExceptionHandler.java
@@ -2,8 +2,10 @@ package com.kbtg.bootcamp.posttest.advice;
 
 import com.kbtg.bootcamp.posttest.exception.ElementNotFoundException;
 import com.kbtg.bootcamp.posttest.exception.IllegalOperationException;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,15 +15,45 @@ import java.util.Map;
 @RestControllerAdvice
 public class ApplicationExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> invalidRequestBodyHandler(MethodArgumentNotValidException e) {
+        Map<String,String> errorMap = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(
+                error -> errorMap.put(error.getField(),error.getDefaultMessage())
+        );
+
+        Map<String,Object> response = new HashMap<>();
+        response.put("errors",errorMap);
+        return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> invalidPathVariableHandler(RuntimeException e) {
+        Map<String,String> response = new HashMap<>();
+
+        // Format of error from path variable can be improved
+        response.put("error",e.getMessage());
+        return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
+    }
+
     @ExceptionHandler({
-            IllegalOperationException.class,
-            ElementNotFoundException.class
+            IllegalOperationException.class
     })
     public ResponseEntity<?> IllegalOperationExceptionHandler(RuntimeException e) {
         Map<String,String> errorMap = new HashMap<>();
         errorMap.put("msg",e.getMessage());
 
         return new ResponseEntity<>(errorMap, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({
+            ElementNotFoundException.class
+    })
+    public ResponseEntity<?> ElementNotFoundExceptionHandler(RuntimeException e) {
+        Map<String,String> errorMap = new HashMap<>();
+        errorMap.put("msg",e.getMessage());
+
+        return new ResponseEntity<>(errorMap, HttpStatus.NOT_FOUND);
     }
 
 
--- posttest/src/main/java/com/kbtg/bootcamp/posttest/controller/UserTicketController.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/controller/UserTicketController.java
@@ -4,14 +4,17 @@ import com.kbtg.bootcamp.posttest.dto.response.PurchaseTicketResponseDTO;
 import com.kbtg.bootcamp.posttest.dto.response.RefundTicketResponseDTO;
 import com.kbtg.bootcamp.posttest.dto.response.UserPurchaseHistoryResponseDTO;
 import com.kbtg.bootcamp.posttest.service.UserTicketService;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
+@Validated
 public class UserTicketController {
 
     private final UserTicketService userTicketService;
@@ -22,12 +25,12 @@ public class UserTicketController {
 
     @PostMapping("/{userId}/lotteries/{ticketId}")
     public ResponseEntity<?> purchaseLottery(
-            @PathVariable
+            @PathVariable(name = "userId")
             @NotBlank(message = "User ID is mandatory")
             @Pattern(regexp = "^[0-9]{10}$",message = "User ID must be 10 digit numbers")
             String userId,
 
-            @PathVariable
+            @PathVariable(name = "ticketId")
             @NotBlank(message = "Lottery number is mandatory")
             @Pattern(regexp = "^[0-9]{6}$",message = "Lottery number must be 6 digit numbers")
             String ticketId
@@ -36,9 +39,9 @@ public class UserTicketController {
         return new ResponseEntity<>(purchaseTicketResponseDTO, HttpStatus.CREATED);
     }
 
-    @GetMapping("{userId}/lotteries")
+    @GetMapping("/{userId}/lotteries")
     public ResponseEntity<?> getPurchaseHistory(
-            @PathVariable
+            @PathVariable(name = "userId")
             @NotBlank(message = "User ID is mandatory")
             @Pattern(regexp = "^[0-9]{10}$",message = "User ID must be 10 digit numbers")
             String userId
@@ -49,12 +52,12 @@ public class UserTicketController {
 
     @DeleteMapping("/{userId}/lotteries/{ticketId}")
     public ResponseEntity<?> refundTicket(
-            @PathVariable
+            @PathVariable(name = "userId")
             @NotBlank(message = "User ID is mandatory")
             @Pattern(regexp = "^[0-9]{10}$",message = "User ID must be 10 digit numbers")
             String userId,
 
-            @PathVariable
+            @PathVariable(name = "ticketId")
             @NotBlank(message = "Lottery number is mandatory")
             @Pattern(regexp = "^[0-9]{6}$",message = "Lottery number must be 6 digit numbers")
             String ticketId
