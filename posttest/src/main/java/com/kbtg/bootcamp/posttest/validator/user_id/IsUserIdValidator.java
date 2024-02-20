46982e7338b3c8b2f78e6f953a94b33eac3ba240
errortime <larb26656@gmail.com>
2024-02-20 09:27:25 +0700

    Refactor (IsUserIdValidator)

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/validator/user_id/IsUserIdValidator.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/validator/user_id/IsUserIdValidator.java
@@ -7,16 +7,6 @@ import jakarta.validation.ConstraintValidatorContext;
 public class IsUserIdValidator implements ConstraintValidator<IsUserId, String> {
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
-        if (value == null || !(value.length() >= 1 && value.length() <= 10)) {
-            return false;
-        }
-
-        try {
-            long number = Long.parseLong(value);
-
-            return number > 0;
-        } catch (NumberFormatException e) {
-            return false;
-        }
+        return value != null && value.matches("\\d{1,10}") && Long.parseLong(value) > 0;
     }
 }
