708b8f321acfecc6f2725673ab8565cc46fbfbda
Pantakarn Kowdokrak <peachoenixz@gmail.com>
2024-02-21 00:08:58 +0700

    feat(Lottery) : implement deleteLottery and refactor class name on another function

--- posttest/src/main/java/com/kbtg/bootcamp/lottery/service/impl/LotteryServiceImpl.java
+++ posttest/src/main/java/com/kbtg/bootcamp/lottery/service/impl/LotteryServiceImpl.java
@@ -10,6 +10,7 @@ import com.kbtg.bootcamp.lottery.repository.UsersRepository;
 import com.kbtg.bootcamp.lottery.request.LotteryRequestDto;
 import com.kbtg.bootcamp.lottery.response.LotteryPurchaseResponseDto;
 import com.kbtg.bootcamp.lottery.response.LotteryResponseDto;
+import com.kbtg.bootcamp.lottery.response.LotteryUserResponseDto;
 import com.kbtg.bootcamp.lottery.service.LotteryService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,7 +21,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
-import static com.kbtg.bootcamp.lottery.lib.Const.*;
+import static com.kbtg.bootcamp.lottery.lib.Constant.*;
 
 @Service
 public class LotteryServiceImpl implements LotteryService {
@@ -37,7 +38,7 @@ public class LotteryServiceImpl implements LotteryService {
 
     @Override
     @Transactional
-    public LotteryResponseDto createLottery(LotteryRequestDto lotteryDTO) {
+    public LotteryResponseDto createLottery(LotteryRequestDto lotteryDTO) throws Exception {
         Optional<Lottery> optionalLottery = lotteryRepository.findById(lotteryDTO.getTicketNumber());
         if (optionalLottery.isPresent()) {
             Lottery lottery = optionalLottery.get();
@@ -61,23 +62,63 @@ public class LotteryServiceImpl implements LotteryService {
 
     @Override
     public LotteryResponseDto getAllLotteryTickets() throws Exception {
-        List<String> ticketNumbers = lotteryRepository.findAll().stream()
+        List<String> ticketNumbers = lotteryRepository
+                .findAll()
+                .stream()
                 .map(Lottery::getTicketNumber)
                 .toList();
         return new LotteryResponseDto(ticketNumbers);
     }
 
+    @Override
+    @Transactional
+    public LotteryResponseDto deleteLottery(String userId, String ticketId) throws Exception {
+        Users user = new Users();
+        user.setUserId(userId);
+        boolean isExist = userTicketRepository.existsByTicketIdAndUserId(user, ticketId);
+        if (!isExist) {
+            throw new NotFoundException(TICKET_NOT_FOUND_OR_USER_ID);
+        }
+        Lottery ticket =  userTicketRepository.findByTicketIdAndUserId(user, Long.valueOf(ticketId));
+        userTicketRepository.deleteLotteryTicketById(user, Long.valueOf(ticketId));
+        Optional<Lottery> optionalLottery = lotteryRepository.findById(ticket.getTicketNumber());
+        if (optionalLottery.isPresent()) {
+            Lottery lottery = optionalLottery.get();
+            lottery.setTicketAmount(lottery.getTicketAmount() + 1);
+            lotteryRepository.save(lottery);
+        }
+        return new LotteryResponseDto(ticket.getTicketNumber());
+    }
+
 
     @Override
     @Transactional
-    public LotteryPurchaseResponseDto purchaseLotteryTicket(final String userId, final String ticketNumber) {
-
-        Optional<Lottery> optionalLottery = lotteryRepository.findById(ticketNumber);
-        if (optionalLottery.isEmpty()) {
-                throw new NotFoundException(String.format(LOTTERY_NOT_FOUND, ticketNumber));
+    public LotteryUserResponseDto getUserLotteryTickets(String userId) throws Exception {
+        Users user = new Users();
+        user.setUserId(userId);
+        List<UserTicket> userTickets = userTicketRepository.findByUserId(user);
+        if (userTickets.isEmpty()) {
+            throw new NotFoundException(String.format(USER_NOT_FOUND, userId));
         }
+        List<String> ticketNumbers = userTickets.stream()
+                .map(UserTicket::getLotteryNumber)
+                .toList();
+        BigDecimal totalPrice = userTicketRepository.getTotalPricePaidByUserId(user);
+        Long totalTicket = userTicketRepository.getTicketCountByUserId(user);
+        return new LotteryUserResponseDto(ticketNumbers, totalPrice, totalTicket);
+    }
+
+    @Override
+    @Transactional
+    public LotteryPurchaseResponseDto purchaseLotteryTicket(final String userId,final String lotteryNumber) throws Exception {
+        Optional<Lottery> optionalLottery = lotteryRepository.findById(lotteryNumber);
+
+        if (optionalLottery.isEmpty()) {
+                throw new NotFoundException(String.format(LOTTERY_NOT_FOUND, lotteryNumber));
+        }
+
         Lottery lottery = optionalLottery.get();
-        validateLotteryAvailable(lottery, ticketNumber);
+        validateLotteryAvailable(lottery, lotteryNumber);
 
 
         Optional<Users> optionalUser = usersRepository.findById(userId);
@@ -86,31 +127,31 @@ public class LotteryServiceImpl implements LotteryService {
         }
 
         decrementTicketAmount(lottery);
-        final UserTicket userTicket = createUserTicket(userId, ticketNumber, lottery.getTicketPrice());
+        final UserTicket userTicket = createUserTicket(userId, lotteryNumber, lottery.getTicketPrice());
         userTicketRepository.save(userTicket);
         return new LotteryPurchaseResponseDto(userTicket.getTicketId());
     }
 
-    private void validateLotteryAvailable(final Lottery lottery, final String ticketNumber) {
+    private void validateLotteryAvailable(Lottery lottery, String ticketNumber) {
         if (lottery.getTicketAmount() <= 0) {
             throw new NotFoundException(String.format(LOTTERY_SOLD_OUT_MESSAGE_FORMAT, ticketNumber));
         }
     }
 
-    private void createUser(final String userId) {
+    private void createUser(String userId) {
         final Users newUser = new Users();
         newUser.setUserId(userId);
         usersRepository.save(newUser);
     }
 
-    private void decrementTicketAmount(final Lottery lottery) {
+    private void decrementTicketAmount(Lottery lottery) {
         lottery.setTicketAmount(lottery.getTicketAmount() - 1);
         lotteryRepository.save(lottery);
     }
 
-    private UserTicket createUserTicket(final String userId, final String ticketNumber, final BigDecimal ticketPrice) {
-        final UserTicket userTicket = new UserTicket();
-        userTicket.setTicketNumber(ticketNumber);
+    private UserTicket createUserTicket(String userId, String ticketNumber, BigDecimal ticketPrice) {
+        UserTicket userTicket = new UserTicket();
+        userTicket.setLotteryNumber(ticketNumber);
         userTicket.setUserId(userId);
         userTicket.setPricePaid(ticketPrice);
         userTicket.setPurchaseDate(LocalDateTime.now());
