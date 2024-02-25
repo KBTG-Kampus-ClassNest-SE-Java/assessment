1cb97a21ce5ea42e916275920cae27652cb86d1e
punPPdev <phatai.punnathon@gmail.com>
2024-02-25 22:55:15 +0700

    remove some properties of test

--- posttest/src/test/java/com/kbtg/bootcamp/posttest/PosttestApplicationTests.java
+++ posttest/src/test/java/com/kbtg/bootcamp/posttest/PosttestApplicationTests.java
@@ -3,6 +3,7 @@ package com.kbtg.bootcamp.posttest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class PosttestApplicationTests {
 
