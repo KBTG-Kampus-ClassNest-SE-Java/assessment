1dc0b58b7cc92e86ae62927bc1e8710f1cf0e159
bencomtech <bencomtech@gmail.com>
2024-02-25 23:10:14 +0700

    response with 201

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/admin/AdminController.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/admin/AdminController.java
@@ -4,6 +4,8 @@ import com.kbtg.bootcamp.posttest.lottery.Lottery;
 import com.kbtg.bootcamp.posttest.lottery.LotteryService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +18,9 @@ public class AdminController {
     private LotteryService lotteryService;
 
     @PostMapping("/lotteries")
-    public Lottery addLottery(@Valid @RequestBody Lottery lottery) {
-        return lotteryService.addLottery(lottery);
+    public ResponseEntity<Lottery> addLottery(@Valid @RequestBody Lottery lottery) {
+        lotteryService.addLottery(lottery);
+
+        return new ResponseEntity<>(lottery, HttpStatus.CREATED);
     }
 }
