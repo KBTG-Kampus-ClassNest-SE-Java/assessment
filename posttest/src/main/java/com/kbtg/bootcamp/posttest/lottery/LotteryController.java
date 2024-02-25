5cf2aa0008e2baf8c8c9cada8b8d123ccfa92d9b
bencomtech <bencomtech@gmail.com>
2024-02-26 02:16:02 +0700

    remove unused import

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/lottery/LotteryController.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/lottery/LotteryController.java
@@ -5,8 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("lotteries")
 public class LotteryController {
