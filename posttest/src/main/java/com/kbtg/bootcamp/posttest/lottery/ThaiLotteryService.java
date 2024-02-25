57b72c33d946de600bd80eda044be896584f985c
bencomtech <bencomtech@gmail.com>
2024-02-25 23:01:06 +0700

    add lottery repo

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/lottery/ThaiLotteryService.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/lottery/ThaiLotteryService.java
@@ -1,11 +1,15 @@
 package com.kbtg.bootcamp.posttest.lottery;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ThaiLotteryService implements LotteryService {
+    @Autowired
+    private LotteryRepository lotteryRepository;
+
     @Override
     public Lottery addLottery(Lottery lottery) {
-        return null;
+        return lotteryRepository.save(lottery);
     }
 }
