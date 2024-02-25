30a3bc37eccbb45cd31d236b3cf7067e62fc0f3a
punPPdev <phatai.punnathon@gmail.com>
2024-02-25 19:51:57 +0700

    add customize API response

---
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/response/CreateLotteryResponse.java
@@ -0,0 +1,13 @@
+package com.kbtg.bootcamp.posttest.response;
+
+public class CreateLotteryResponse {
+    private String ticket;
+
+    public String getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(String ticket) {
+        this.ticket = ticket;
+    }
+}
