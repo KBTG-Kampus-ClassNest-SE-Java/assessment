3a4a6d26a9a4686a135d242e1fe9772300d4a896
punPPdev <phatai.punnathon@gmail.com>
2024-02-26 14:02:02 +0700

    add UserTicketDetailResponse for endpoints /users/{userId}/lotteries

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/response/UserTicketDetailResponse.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/response/UserTicketDetailResponse.java
@@ -1,2 +1,38 @@
-package com.kbtg.bootcamp.posttest.response;public class UserTicketDetail {
+package com.kbtg.bootcamp.posttest.response;
+
+import java.util.List;
+
+public class UserTicketDetailResponse {
+
+    private List<String> tickets;
+    private Integer count;
+    private Integer cost;
+
+    public UserTicketDetailResponse() {
+
+    }
+
+    public List<String> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<String> tickets) {
+        this.tickets = tickets;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
 }
