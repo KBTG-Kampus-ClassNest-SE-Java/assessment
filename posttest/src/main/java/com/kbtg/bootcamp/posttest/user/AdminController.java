a79d34fb464c1cf519370a8acc31a8b4b3f406fb
punPPdev <phatai.punnathon@gmail.com>
2024-02-26 16:44:48 +0700

    add created response status

--- posttest/src/main/java/com/kbtg/bootcamp/posttest/user/AdminController.java
+++ posttest/src/main/java/com/kbtg/bootcamp/posttest/user/AdminController.java
@@ -5,10 +5,7 @@ import com.kbtg.bootcamp.posttest.response.CreateLotteryResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -22,6 +19,7 @@ public class AdminController {
     }
 
     @PostMapping("/lotteries")
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<CreateLotteryResponse> createLottery(@Valid @RequestBody LotteryRequestDto requestDto) {
 
         this.adminService.createLottery(requestDto);
